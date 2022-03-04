99a7b43a656a2a62c0a149ac89f379c3ab490d4f
Gouzhzong1223 <1162864960@qq.com>
2022-03-04 15:00:32 +0800

    update

--- app/src/main/java/com/gouzhong1223/androidtvtset_1/actives/VideoActivity.java
+++ app/src/main/java/com/gouzhong1223/androidtvtset_1/actives/VideoActivity.java
@@ -3,14 +3,23 @@ package com.gouzhong1223.androidtvtset_1.actives;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 import com.gouzhong1223.androidtvtset_1.R;
 
 public class VideoActivity extends Activity {
 
+    private VideoView videoView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+        initView();
+    }
+
+    private void initView() {
+        videoView = findViewById(R.id.videoView);
     }
 }
