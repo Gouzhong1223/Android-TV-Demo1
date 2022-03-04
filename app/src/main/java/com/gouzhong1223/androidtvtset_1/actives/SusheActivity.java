d692e0788ad7e858cea7f3237ed62be4bd3a695e
Gouzhzong1223 <1162864960@qq.com>
2022-03-04 15:16:08 +0800

    新增宿舍轮播图组件

--- app/src/main/java/com/gouzhong1223/androidtvtset_1/actives/SusheActivity.java
+++ app/src/main/java/com/gouzhong1223/androidtvtset_1/actives/SusheActivity.java
@@ -1,16 +1,39 @@
 package com.gouzhong1223.androidtvtset_1.actives;
 
-import android.app.Activity;
 import android.os.Bundle;
 
 
-import com.gouzhong1223.androidtvtset_1.R;
+import androidx.fragment.app.FragmentActivity;
 
-public class SusheActivity extends Activity {
+import com.gouzhong1223.androidtvtset_1.R;
+import com.gouzhong1223.androidtvtset_1.banner.adapter.ImageAdapter;
+import com.gouzhong1223.androidtvtset_1.banner.bean.BannerDataBean;
+import com.youth.banner.Banner;
+import com.youth.banner.indicator.CircleIndicator;
+
+public class SusheActivity extends FragmentActivity {
+
+    private Banner banner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sushe);
+        initView();
+    }
+
+    private void initView() {
+        banner = findViewById(R.id.banner);
+        useBanner();
+    }
+
+    /**
+     * 播放轮播图
+     */
+    public void useBanner() {
+        //添加生命周期观察者
+        banner.addBannerLifecycleObserver(this)
+                .setAdapter(new ImageAdapter(BannerDataBean.getTestData()))
+                .setIndicator(new CircleIndicator(this));
     }
 }
